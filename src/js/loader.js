f0a328bf0125a927ca5ab8c1918bef874e1db408
Ritter19 <joey.barbacena@gmail.com>
2025-08-15 09:13:07 +0800

    Initial commit

---
+++ src/js/loader.js
@@ -0,0 +1,11 @@
+const preloader = document.querySelector('.preloader');
+const body = document.querySelector('body');
+
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    preloader.remove();
+    body.style.overflowY = 'scroll';
+  }, 1500);
+});
+
+//Done
