6ef0dcdabae70d1a42d27c5604423698191a0110
Ritter19 <joey.barbacena@gmail.com>
2025-08-20 18:14:49 +0800

    last last last

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,24 +1,11 @@
-// const preloader = document.querySelector('.preloader');
-// const body = document.querySelector('body');
-
-// window.addEventListener('load', () => {
-//   setTimeout(() => {
-//     preloader.remove();
-//     body.style.overflowY = 'scroll';
-//   }, 1500);
-// });
-
-// //Done
+const preloader = document.querySelector('.preloader');
+const body = document.querySelector('body');
 
 window.addEventListener('load', () => {
-  const preloader = document.querySelector('.preloader');
-  if (preloader) {
-    // Keep loader for 1 second after page load
-    setTimeout(() => {
-      preloader.style.opacity = '0';
-      setTimeout(() => {
-        preloader.style.display = 'none';
-      }, 500); // fade-out duration
-    }, 1000); // delay before hiding (1000ms = 1s)
-  }
+  setTimeout(() => {
+    preloader.remove();
+    body.style.overflowY = 'scroll';
+  }, 1500);
 });
+
+// Done
