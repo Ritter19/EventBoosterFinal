6b8598d1e11baa227df6c89fad5daea4109f7c43
Ritter19 <joey.barbacena@gmail.com>
2025-08-20 17:51:21 +0800

    Update
    Last update

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,11 +1,24 @@
-const preloader = document.querySelector('.preloader');
-const body = document.querySelector('body');
+// const preloader = document.querySelector('.preloader');
+// const body = document.querySelector('body');
+
+// window.addEventListener('load', () => {
+//   setTimeout(() => {
+//     preloader.remove();
+//     body.style.overflowY = 'scroll';
+//   }, 1500);
+// });
+
+// //Done
 
 window.addEventListener('load', () => {
-  setTimeout(() => {
-    preloader.remove();
-    body.style.overflowY = 'scroll';
-  }, 1500);
+  const preloader = document.querySelector('.preloader');
+  if (preloader) {
+    // Keep loader for 1 second after page load
+    setTimeout(() => {
+      preloader.style.opacity = '0';
+      setTimeout(() => {
+        preloader.style.display = 'none';
+      }, 500); // fade-out duration
+    }, 1000); // delay before hiding (1000ms = 1s)
+  }
 });
-
-//Done
